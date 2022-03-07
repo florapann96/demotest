25c341f3b5de4b1f81b7f44fbbf5fa16532541b8
Pann Phway <pppmeemee@gmail.com>
2022-03-07 19:51:31 +0800

    new commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,13 @@
-module.exports = {
+////module.exports = {
 
-    /*database: 'mongodb://localhost:27017/myuser'*/
-    database: 'mongodb+srv://pppadmin:testing123@cluster0.pzqdl.mongodb.net/bookdemo?retryWrites=true&w=majority'
+////    /*database: 'mongodb://localhost:27017/myuser'*/
+////    database: 'mongodb+srv://pppadmin:testing123@cluster0.pzqdl.mongodb.net/bookdemo?retryWrites=true&w=majority'
+////}
+if (process.env.NODE_ENV == "production") {
+
+    module.exports = require("./prod")
+
+} else {
+
+    module.exports = require("./dev")
 }
